3a7ea2c5d5244024c28cd808b8e4043e771228f0
JenJen <jennifer7793@gmail.com>
2022-02-19 02:13:53 +0800

    set article controller, new & index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,18 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+  end
 
   def new
+    @blog = Blog.find_by(id:params[:blog_id])
     @article = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
+    blog = Blog.find_by(id:params[:id])
+    @article = blog.articles.build(article_params)
     if @article.save
-      redirect_to articles_path
+      redirect_to blog_articles_path
     else
       render :new
     end
@@ -18,11 +21,11 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find_by(id:params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to blog_articles_path
   end
   
   private
   def article_params
-    params.require(:article).permit(:title, :content, :blog_id)
+    params.require(:article).permit(:title, :content)
   end
 end
