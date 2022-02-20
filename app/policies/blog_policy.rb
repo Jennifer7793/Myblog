1d59c7a2082a39bbeffdedd2cfb30a4ad24b6498
JenJen <jennifer7793@gmail.com>
2022-02-20 12:31:39 +0800

    wip

--- app/policies/blog_policy.rb
+++ app/policies/blog_policy.rb
@@ -1,16 +1,16 @@
 class BlogPolicy < ApplicationPolicy
-  attr_reader :user, :Blog
+  attr_reader :user, :blog
   def initialize(user, blog)
     @user = user
     @blog = blog
   end
 
   def edit?
-    user.user_blogs.find_by(blog_id: @blog.id).owner? ||
-    user.user_blogs.find_by(blog_id: @blog.id).editor?
   end
 
+  def update?
+  end
+    
   def destroy?
-    user.user_blogs.find_by(blog_id: @blog.id).owner?
   end
 end
