bb0f1c0409f25f650bb3fcc2f9098c901dc2950c
JenJen <jennifer7793@gmail.com>
2022-02-21 21:14:40 +0800

    add publish column and set blog#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_165134) do
+ActiveRecord::Schema.define(version: 2022_02_21_081731) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_20_165134) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "blog_id"
+    t.integer "publish"
     t.index ["blog_id"], name: "index_articles_on_blog_id"
   end
 
