37ba6a363d74131081eee17afaf006618df9d57e
JenJen <jennifer7793@gmail.com>
2022-02-23 02:36:41 +0800

    remove uuser refernces from blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_081731) do
+ActiveRecord::Schema.define(version: 2022_02_22_183314) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(version: 2022_02_21_081731) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "user_blogs", force: :cascade do |t|
