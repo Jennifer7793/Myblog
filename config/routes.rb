9fbb28dfa53459fa6871795c8e53a76571b9927a
JenJen <jennifer7793@gmail.com>
2022-02-24 21:32:29 +0800

    fix: user routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :blogs do
     resources :articles, shallow: true
   end
