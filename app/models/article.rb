0d326dd60367e473e6e6bee557580ffdf3324681
JenJen <jennifer7793@gmail.com>
2022-02-25 09:08:38 +0800

    add validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, :content, presence: true
+
   belongs_to :blog
   enum publish: { yes: 0, no: 1 }
   scope :published, -> { where(publish: 'yes') }
